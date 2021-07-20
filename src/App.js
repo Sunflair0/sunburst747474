8cc01412e12d97ab9dd0d187ac81967b61774dd8
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-20 13:16:08 -0500

    app.js files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [user,setUser] =useState(null);
 
+    const signUp =useCallback((email, password) => {
+//TODO IMPLEMENT
+    });      
+  
+const loginWithEmail =useCallback((email, password) => {
+//TODO IMPLEMENT
+    });    
+
+const loginWithGoogle =useCallback((email, password) => {
+//TODO IMPLEMENT
+    });    
+const logout = useCallback((email, password) => {
+//TODO IMPLEMENT
+    });    
+return <div className="App"></div>;
+}
 export default App;
