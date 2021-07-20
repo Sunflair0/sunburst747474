116c4b0b596d9ad521d915c6385318b784b9aab7
Rhonda Warner <sunflair_2000@yahoo.com>
2021-07-20 15:34:21 -0500

    login done

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
-const Login = () => {
-  return <div>Login Works</div>;
+const Signup = ({ signUp }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            if (email.length > 5 && password.length > 5) {
+              signUp(email, password);
+            }
+          }}
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default Signup;
